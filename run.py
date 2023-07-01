123b8849655a46c5ed72aea9ad30fa027b26e68a
Mark Dunne <Markdunne117@gmail.com>
2023-07-01 16:41:02 +0000

    Add main menu graphics

--- run.py
+++ run.py
@@ -1,2 +1,19 @@
 import random
 from words import word_list
+
+
+print(' _')
+print('| |')
+print('| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __')
+print("| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ ")
+print('| | | | (_| | | | | (_| | | | | | | (_| | | | |')
+print('|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|')
+print('                    __/ |')
+print('                   |___/')
+print('          +--------------------------+')
+print('          |                          |')
+print('          |       Play Game: 1       |')
+print('          |                          |')
+print('          |       Game Rules: 2      |')
+print('          |                          |')
+print('          +--------------------------+')
